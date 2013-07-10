6c79a6c94999c130bde51057996ce5763abb5a5b
Josh McKeany <jmckeany@gmail.com>
2013-07-10 12:27:00 +0000

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -3,10 +3,21 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require('fs');
+	fs.readFileSync('./index.html', function (err, data) {
+	  if (err) response.send(err); //throw err;
+	  console.log(data);
+	  response.send(data);
+	});
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readFile = function() {
+
+		
+};
